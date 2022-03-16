1d4a9ce9ec1571b8f6784e7d92424c3d57346356
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-03-16 10:58:57 +0700

    user services

---
+++ services/user.services.js
@@ -0,0 +1,91 @@
+import User from "../models/user.model";
+
+
+
+async function getUser(userId) {
+    try {
+        const user = await User.findById(userId)
+        if(!user) {
+            return {
+                success: false,
+                message: "User not found!",
+                data: null
+            }
+        }
+
+      return {
+        success: true,
+        message: "Get user successful!",
+        data: user,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+        data: null
+      };
+    }
+}
+
+async function registerUser(username, password, email) {
+    try {
+        const user = await User.create({username, password, email})
+        if(!user) {
+            return {
+                success: false,
+                message: "Error when create user",
+                data: null
+            };
+        }
+
+        return {
+            success: true,
+            message: "Create user successful!",
+            data: user
+        };
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message,
+            data: null
+        };
+    }
+}
+
+async function login(email, password) {
+    try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if(!user) {
+            return {
+                success: false,
+                message: "Invalid credentials",
+                data: null
+            };
+        }
+
+        const passwordMatch = await user.matchPasswords(password);
+
+        if (!passwordMatch) {
+            return {
+                success: false,
+                message: "Invalid credentials",
+                data: null
+            };
+        }
+
+        return {
+            success: true,
+            message: "Login successfull!",
+            data: user
+        }
+
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message,
+            data: null
+        };
+    }
+}
+  
