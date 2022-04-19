d8b3830c88f05eb980c58bcc0cfe8e34a934bd28
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-04-19 14:52:08 +0700

    deploy 1

--- routes/room.router.js
+++ routes/room.router.js
@@ -4,10 +4,11 @@ import { RoomController } from "../controllers/room.controller.js"
 const RoomRouter = express.Router()
 
 RoomRouter
-    .get("/",RoomController.getRoom)
-    .post("/",RoomController.postRoom)
-    .put("/",RoomController.updateRoom)
-    .delete("/",RoomController.deleteRoom)
+    .route("/")
+    .get(RoomController.getRoom)
+    .post(RoomController.postRoom)
+    .put(RoomController.updateRoom)
+    .delete(RoomController.deleteRoom)
 
 
 RoomRouter.get("/search", RoomController.searchRoom);
