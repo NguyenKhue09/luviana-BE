efda9a461df3cb4541070b19a3944b8fa2ed16e9
KhoiLe <Khoilecontact@gmail.com>
2022-04-19 15:12:37 +0700

    add try catch blocks for room controller

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -2,88 +2,137 @@ import { add } from "nodemon/lib/rules";
 import { RoomServices } from "../services/room.services.js";
 
 async function getRoom(req, res) {
-    const { highToLow } = req.query;
-    if (highToLow) {
-        const response = await RoomServices.getRoomBySortPriceReverse()
-        if (response.success) {
-            return res.json(response)
+    try {
+        const { highToLow } = req.query;
+        if (highToLow) {
+            const response = await RoomServices.getRoomBySortPriceReverse()
+            if (response.success) {
+                return res.json(response)
+            } else {
+                return res.status(500).json(response)
+            }
         } else {
-            return res.status(500).json(response)
+            const response = await RoomServices.getRoomBySortPrice()
+            if (response.success) {
+                return res.json(response)
+            } else {
+                return res.status(500).json(response)
+            }
         }
-    } else {
-        const response = await RoomServices.getRoomBySortPrice()
-        if (response.success) {
-            return res.json(response)
-        } else {
-            return res.status(500).json(response)
-        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
+    
 }
 
 async function searchRoom (req, res) {
-    const { checkinDate, checkoutDate, people, city } = req.body;
+    try {
+        const { checkinDate, checkoutDate, people, city } = req.body;
 
-    var rooms = await RoomServices.searchRoom(checkinDate, checkoutDate, people, city)
+        var rooms = await RoomServices.searchRoom(checkinDate, checkoutDate, people, city)
 
-    if (rooms.success) {
-        if (result.data) return res.json(result)
-        else return res.status(404).json(result)
-    } else {
-        return res.status(500).json(result)
+        if (rooms.success) {
+            if (result.data) return res.json(result)
+            else return res.status(404).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
 }
 
 async function postRoom (req, res) {
-    const { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities} = req.body;
+    try {
+        const { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities} = req.body;
 
-    var newData = { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities};
+        var newData = { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities};
 
-    var addNewRoom = await RoomServices.addNewRoom(newData);
+        var addNewRoom = await RoomServices.addNewRoom(newData);
 
-    if (addNewRoom.success) {
-        if (result.data) return res.json(result)
-        else return res.status(404).json(result)
-    } else {
-        return res.status(500).json(result)
+        if (addNewRoom.success) {
+            if (result.data) return res.json(result)
+            else return res.status(404).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
 }
 
 async function updateRoom (req, res) {
-    const roomId = req.body.roomId;
-    const { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities} = req.body;
+    try {
+        const roomId = req.body.roomId;
+        const { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities} = req.body;
 
-    var updateData = { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities};
+        var updateData = { apartmentId, price, description, capacity, rating, thumbnail, pictures, isAvailable, facilities};
 
-    var updateRoom = await RoomServices.updateRoom(roomId, updateData);
+        var updateRoom = await RoomServices.updateRoom(roomId, updateData);
 
-    if (updateRoom.success) {
-        if (result.data) return res.json(result)
-        else return res.status(404).json(result)
-    } else {
-        return res.status(500).json(result)
+        if (updateRoom.success) {
+            if (result.data) return res.json(result)
+            else return res.status(404).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
 }
 
 async function deleteRoom (req, res) {
-    const roomId = req.body.roomId;
+    try {
+        const roomId = req.body.roomId;
 
-    var deleteRoom = await RoomServices.deleteRoom(roomId);
+        var deleteRoom = await RoomServices.deleteRoom(roomId);
 
-    if (deleteRoom.success) {
-        if (result.data) return res.json(result)
-        else return res.status(404).json(result)
-    } else {
-        return res.status(500).json(result)
+        if (deleteRoom.success) {
+            if (result.data) return res.json(result)
+            else return res.status(404).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
 }
 
 async function searchRoom(req, res) {
-    const { checkinDate, checkoutDate, people, city } = req.query;
-    const response = RoomController.searchRoom(checkinDate, checkoutDate, people, city);
-    if (response.success) {
-        res.json(response);
-    } else {
-        res.status(400).json(response);
+    try {
+        const { checkinDate, checkoutDate, people, city } = req.query;
+        const response = RoomController.searchRoom(checkinDate, checkoutDate, people, city);
+        if (response.success) {
+            res.json(response);
+        } else {
+            res.status(400).json(response);
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+            data: null
+        })
     }
 }
 
