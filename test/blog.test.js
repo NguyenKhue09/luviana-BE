a38646ee0481833c24ebc12eecb35b8dce45aa2e
KhoiLe <Khoilecontact@gmail.com>
2022-05-18 10:13:37 +0700

    test blog fail cases

--- test/blog.test.js
+++ test/blog.test.js
@@ -55,4 +55,64 @@ describe('Good blog result', function() {
         expect(res.header['content-type']).toBe('application/json; charset=utf-8')
         expect(res.status).toBe(200)
     });
+
+    test('Respond to update a blog', async() => {
+        const updateBlog = {
+            "data": {
+                "content": "This is a testing blog",
+                "pictures": "https://assets.grab.com/wp-content/uploads/sites/11/2020/09/30172754/Hotels_Booking_1920x675.jpg"
+            }, 
+            "blogId": "62845bfb638d4091909b7136"
+        };
+
+        const res = await request(app)
+        .put('/blog/update')
+        .send(updateBlog)
+
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8')
+        expect(res.status).toBe(200)
+    });
+});
+
+describe('Fail test result for blog', function() {
+    test('Fail respond to add new blog', async() => {
+        const newBlog = {
+            // "author": "623442ba82b88524caae2232",
+            "content": "This is a testing blog",
+            "pictures": "https://assets.grab.com/wp-content/uploads/sites/11/2020/09/30172754/Hotels_Booking_1920x675.jpg",
+            "date": "2022-05-30"
+        };
+
+        const res = await request(app)
+        .post('/blog')
+        .send(newBlog)
+
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8')
+        expect(res.status).toBe(400)
+    });
+
+    test('Fail respond to get blog by id', async() => {
+        const res = await request(app)
+        .get('/blog/detail/6284avcb638d4091909b7123')
+
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8')
+        expect(res.status).toBe(500)
+    });
+
+    test('Respond to update a blog', async() => {
+        const updateBlog = {
+            "data": {
+                "content": "This is a testing blog",
+                "pictures": "https://assets.grab.com/wp-content/uploads/sites/11/2020/09/30172754/Hotels_Booking_1920x675.jpg"
+            }, 
+            "blogId": "62845bfb638d4091909bmamv"
+        };
+
+        const res = await request(app)
+        .put('/blog/update')
+        .send(updateBlog)
+
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8')
+        expect(res.status).toBe(500)
+    });
 });
