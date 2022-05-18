7c60546231e747a6f6bad00932fde7a09b605145
Nguyen Huu Long <longnh.0601@gmail.com>
2022-05-18 22:50:12 +0700

    remove coverate ?? 😀 ??

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,4 +1,11 @@
 import { BlogService } from '../services/blog.services.js';
+import cloudinaryInstance from "cloudinary"
+const cloudinary = cloudinaryInstance.v2
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
 
 async function addNewBlog(req, res) {
     const { author, content, pictures, date } = req.body;
