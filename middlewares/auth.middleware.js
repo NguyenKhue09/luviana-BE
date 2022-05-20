ba6273cf853a3fa6d4d9eb00d6cf73b0b01070b8
KhoiLe <Khoilecontact@gmail.com>
2022-05-20 09:48:40 +0700

    fix auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,21 +7,29 @@ module,exports.requireUser = (req, res, next) => {
     var authHeader = req.headers['authorizationtoken'];
 
     if (authHeader && authHeader.split(' ')[0] !== 'Bearer') resHelper(res, 401, {error: 'Unauthorized'}, 'Unauthorized');
-    if (_.isNil(authHeader)) return resHelper(res, 401, {error: 'Unauthorized'}, 'Unauthorized');
     
     var token = authHeader.split(' ')[1];
     var decodedToken = null;
     
     try {
-      decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      var currentUser = await User.get({name: decodedToken.userName});
-      if (_.isNil(currentUser)) return resHelper(res, 400, {error: 'bad request'}, 'Bad request');
-      res.locals.user = currentUser;
+      decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+      req.userId = decodedToken._id
     } catch (error) {
-      return resHelper(res, 400, {error: 'bad request'}, 'Bad request');
+      res.status(400).json({
+        success: false,
+        message: "Authorize Failed",
+        data: null
+      })
     }
 
-    if (_.isNil(decodedToken)) return resHelper(res, 400, {error: 'bad request'}, 'Bad request');
+    if (decodedToken == null) {
+      res.status(400).json({
+        success: false,
+        message: "Authorize Failed",
+        data: null
+      })
+    }
+    
     next();
   }
 
