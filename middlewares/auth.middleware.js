bdd17c1a82fab56f1a7ec4e25e21fb8b805c0299
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-05-20 10:31:20 +0700

    deploy

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,42 +1,44 @@
-const userModel = require('../models/user.model.js')
 
 //Function: middleware of user rights  
 //Input: userId from cookie
 //Output: allow it to go to the next function or not 
-module.exports.requireUser = (req, res, next) => {
+export const requireUser = (req, res, next) => {
     var authHeader = req.headers['authorizationtoken'];
 
-    if (authHeader && authHeader.split(' ')[0] !== 'Bearer') resHelper(res, 401, {error: 'Unauthorized'}, 'Unauthorized');
+    if (authHeader && authHeader.split(' ')[0] !== 'Bearer') return  res.status(400).json({
+      success: false,
+      message: "Authorize Failed",
+      data: null
+    })
     
     var token = authHeader.split(' ')[1];
     var decodedToken = null;
     
     try {
       decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-      req.userId = decodedToken._id
-    } catch (error) {
-      res.status(400).json({
-        success: false,
-        message: "Authorize Failed",
-        data: null
-      })
-    }
+      if (decodedToken == null) {
+        return res.status(400).json({
+          success: false,
+          message: "Authorize Failed",
+          data: null
+        })
+      }
 
-    if (decodedToken == null) {
-      res.status(400).json({
+      req.userId = decodedToken._id
+      next();
+    } catch (error) {
+      return res.status(400).json({
         success: false,
         message: "Authorize Failed",
         data: null
       })
-    }
-    
-    next();
+    }  
   }
 
 //Function: middleware of admin rights  
 //Input: adminId from cookie 
 //Output:  allow it to go to the next function or not 
-module.exports.requireAdmin = (req, res, next) => { 
+export const requireAdmin = (req, res, next) => { 
     var authHeader = req.headers['authorizationAdmintoken'];
 
     if (authHeader && authHeader.split(' ')[0] !== 'Bearer') resHelper(res, 401, {error: 'Unauthorized'}, 'Unauthorized');
@@ -47,7 +49,7 @@ module.exports.requireAdmin = (req, res, next) => {
     
     try {
       decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      var currentUser = await User.get({name: decodedToken.userName});
+      //var currentUser = await User.get({name: decodedToken.userName});
       if (_.isNil(currentUser)) return resHelper(res, 400, {error: 'bad request'}, 'Bad request');
       res.locals.admin = currentUser;
     } catch (error) {
