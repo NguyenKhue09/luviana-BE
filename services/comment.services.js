c1d6b3882239f78396ad7b5e07696257822b2e6f
Nguyen Huu Long <longnh.0601@gmail.com>
2022-05-27 22:42:50 +0700

    push comment fix

--- services/comment.services.js
+++ services/comment.services.js
@@ -23,7 +23,10 @@ async function addComment(author, blogId, content) {
             date: new Date()
         }, { session })
         await comment.save()
-        result = await Blog.findOneAndUpdate({ _id: mongoose.Types.ObjectId(blogId) }, { $push: { comments: comment._id } }, { session, returnDocument: "after" })
+        result = await Blog.findOneAndUpdate({ 
+            _id: mongoose.Types.ObjectId(blogId) }, 
+            { $push: { comments: { $each: [comment._id], $position: 0 } } }, 
+            { session, returnDocument: "after" })
         if (!result) {
             throw new Error("Comment not saved!")
         }
