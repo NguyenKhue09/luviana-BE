c150f4c5c03897c051d8264717052cc75cf2a728
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-05-30 16:15:35 +0700

    revenue

--- controllers/revenue.controller.js
+++ controllers/revenue.controller.js
@@ -21,6 +21,49 @@ async function getMonthlyRevenue(req, res) {
     }
 }
 
+async function getYearlyRevenue(req, res) {
+    try {
+        
+        const { year } = req.query
+        const result = await RevenueServices.getYearlyRevenue(parseInt(year))
+        
+        if(result.success) {
+            return res.status(200).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+            data: null
+        })
+    }
+}
+
+async function getAllYearlyRevenue(req, res) {
+    try {
+        
+        const result = await RevenueServices.getAllYearlyRevenue()
+        
+        if(result.success) {
+            return res.status(200).json(result)
+        } else {
+            return res.status(500).json(result)
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+            data: null
+        })
+    }
+}
+
 export const RevenueController = {
-    getMonthlyRevenue
+    getMonthlyRevenue,
+    getYearlyRevenue,
+    getAllYearlyRevenue
 }
