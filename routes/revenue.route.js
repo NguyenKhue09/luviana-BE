56a56101e3526f7da8346bfe167f4008d4fe8118
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-05-30 16:30:58 +0700

    revenue

--- routes/revenue.route.js
+++ routes/revenue.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { RevenueController } from "../controllers/revenue.controller.js";
+import { AuthMiddleWare } from "../middlewares/auth.middleware.js";
 
 
 const RevenueRouter = express.Router()
 
-RevenueRouter.get("/monthly", RevenueController.getMonthlyRevenue)
-RevenueRouter.get("/yearly", RevenueController.getYearlyRevenue)
-RevenueRouter.get("/all-yearly", RevenueController.getAllYearlyRevenue)
+RevenueRouter.get("/monthly", AuthMiddleWare.requireAdmin, RevenueController.getMonthlyRevenue)
+RevenueRouter.get("/yearly", AuthMiddleWare.requireAdmin, RevenueController.getYearlyRevenue)
+RevenueRouter.get("/all-yearly", AuthMiddleWare.requireAdmin, RevenueController.getAllYearlyRevenue)
 
 export { RevenueRouter };
