485f48b5b5408739ca4ad7dc8b5d991f5ec11269
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-06-02 16:41:59 +0700

    fix api room

--- services/room.services.js
+++ services/room.services.js
@@ -649,7 +649,7 @@ async function addNewRoom(data) {
 
 async function updateRoom(roomId, data) {
   try {
-    delete data.isDisable;
+    //delete data.isDisable;
 
     const result = await Room.findByIdAndUpdate(roomId, data);
 
